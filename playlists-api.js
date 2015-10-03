f37ee2cbbeebfc108b27f79b6b55b213381b4cb6
unknown <alextsang@live.com>
2015-10-03 23:26:01 +0800

    Removed ASX playlist support.

--- playlists-api.js
+++ playlists-api.js
@@ -4,8 +4,6 @@
  * References:
  * http://en.wikipedia.org/wiki/PLS_(file_format)
  * http://schworak.com/blog/e41/extended-pls-plsv2/
- * http://en.wikipedia.org/wiki/Advanced_Stream_Redirector
- * https://msdn.microsoft.com/en-us/library/dd564668(v=vs.85).aspx
  *
  * @author alextsang@live.com
  */
@@ -105,83 +103,9 @@ module.exports = (function () {
         response.write('Version=2');
         response.end();
       });
-    },
-
-    /**
-     * Generates music playlist in ASX format.
-     *
-     * @param {Object} request HTTP request.
-     * @param {Object} response HTTP response.
-     */
-    getPlaylistAsASX = function (request, response) {
-      var port = null,
-        tracksDirectory = request.app.locals.tracksDirectory,
-        token = authenticationUtil.generateToken(),
-        asxObject = {
-          asx: [
-            {
-              _attr: {
-                version: '3.0'
-              }
-            },
-            {
-              title: 'Music'
-            }
-          ]
-        };
-      fs.readdir(tracksDirectory, function (error, files) {
-        if (error !== null) {
-          console.error('Unable to get read tracks directory.');
-          console.error(error);
-          response.statusCode = 500;
-          response.type('text/plain');
-          response.end('INTERNAL_ERROR');
-          return;
-        }
-        response.statusCode = 200;
-        response.type('video/x-ms-asf');
-        if (request.protocol === 'http') {
-          if (request.app.locals.httpPortInUrl !== null) {
-            port = request.app.locals.httpPortInUrl;
-          }
-        } else if (request.protocol === 'https') {
-          if (request.app.locals.httpsPortInUrl !== null) {
-            port = request.app.locals.httpsPortInUrl;
-          }
-        }
-        files.forEach(function (file) {
-          var url = buildTrackUrl({
-              protocol: request.protocol,
-              hostname: request.hostname,
-              port: port,
-              appBasePath: request.app.locals.appBasePath,
-              trackName: file,
-              token: token
-            });
-          asxObject.asx.push({
-            entry: [
-              {
-                title: file
-              },
-              {
-                ref: [
-                  {
-                    _attr: {
-                      href: url
-                    }
-                  }
-                ]
-              }
-            ]
-          });
-        });
-        response.write(xml(asxObject));
-        response.end();
-      });
     };
 
   router.get('/music.pls', getPlaylistAsPLS);
-  router.get('/music.asx', getPlaylistAsASX);
 
   return router;
 }());
